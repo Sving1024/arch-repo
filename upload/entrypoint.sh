be178a0c2886aa9cf2c71e45c46699bafc4d90e1
Sving1024 <sving1024@outlook.com>
2025-06-14 22:56:38 +0800

    fix path

--- upload/entrypoint.sh
+++ upload/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
-init_path=$PWD
-cd upload_packages
+init_path=$(
+    cd "$(dirname $0)"
+    pwd
+)
 
 echo "$RCLONE_CONFIG_NAME"
 
 if [ ! -f ~/.config/rclone/rclone.conf ]; then
     mkdir --parents ~/.config/rclone
-    echo "$RCLONE_CONFIG_CONTENT" >> ~/.config/rclone/rclone.conf
+    echo "$RCLONE_CONFIG_CONTENT" >>~/.config/rclone/rclone.conf
 fi
 
 echo "::group::Uploading to remote"
-python3 $init_path/upload/upload.py 
+python3 $init_path/upload/upload.py
 echo "::endgroup::"
